1a23e902821769bc3a638fea09b5f44ea00a47ee
ngvbach2000 <ngvbach2000@gmail.com>
2022-03-21 14:08:18 +0700

    Update FE

--- NeighborlyFrontEnd/settings.py
+++ NeighborlyFrontEnd/settings.py
@@ -1,7 +1,9 @@
+import os
+
 #--------- Flask settings
-SERVER_HOST = '0.0.0.0' # Update this for the appropriate front-end website when up
-SERVER_PORT = 5000
-FLASK_DEBUG = True # Do not use debug mode in prod
+SERVER_HOST = os.get('SERVER_HOST', 'localhost') # Update this for the appropriate front-end website when up
+SERVER_PORT = int(os.get('SERVER_PORT', '5555'))
+FLASK_DEBUG = False # Do not use debug mode in prod
 
 # Flask-Restplus settings
 SWAGGER_UI_DOC_EXPANSION = 'list'
